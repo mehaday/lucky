b91ee6d1f24e84a8a867d07193fc774146f618c0
mehaday <93436872+mehaday@users.noreply.github.com>
2023-06-26 01:00:03 +0700

    update

--- app/src/main/java/com/example/navigationcomponentsexample/KetQua.kt
+++ app/src/main/java/com/example/navigationcomponentsexample/KetQua.kt
@@ -6,38 +6,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 
 //class KetQua : Fragment(),View.OnClickListener {
 class KetQua : Fragment() {
     var navController: NavController? = null
-
+    private lateinit var webView: WebView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ket_qua, container, false)
+        val view = inflater.inflate(R.layout.fragment_ket_qua, container, false)
+        webView = view.findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        // Cấu hình WebView
+        webView.settings.apply {
+            javaScriptEnabled = true // Bật hỗ trợ JavaScript nếu cần
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
+        // Load trang web
+        val url = "http://api.xosodo.vn/webview/soicaudetail?loto=90&p1=7&p2=19&limit=4&provinceId=1&dayPrize=08/11/2018&type=1"
+        webView.loadUrl(url)
+
+        // Xóa thẻ sau khi trang web được tải hoàn thành
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                removeElementFromWebView("/html/body/section/div[1]")
+            }
+        }
+
+        return view
     }
+    private fun removeElementFromWebView(xpath: String) {
+        val js = """
+            var element = document.evaluate('$xpath', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.parentNode.removeChild(element);
+            }
+        """.trimIndent()
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        navController = Navigation.findNavController(view)
-//        view.findViewById<Button>(R.id.next_btn).setOnClickListener(this)
-//        view.findViewById<Button>(R.id.cancel_btn).setOnClickListener(this)
-//    }
-//
-//    @SuppressLint("UseRequireInsteadOfGet")
-//    override fun onClick(v: View?) {
-//        when(v!!.id){
-//            R.id.next_btn -> navController!!.navigate(R.id.action_chooseRecipientFragment_to_specifyAmountFragment)
-//            // cach 1 : R.id.cancel_btn -> navController!!.navigate(R.id.main Fragment)
-//            //cach 2
-//            R.id.cancel_btn -> activity!!.onBackPressed()
-//        }
-//    }
-
+        webView.evaluateJavascript(js, null)
+    }
 }
